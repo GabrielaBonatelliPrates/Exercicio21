4b15de259f68b91cbf515b5c1949def81998e7e5
rafae <rafae@RaFeIGa>
2025-04-06 11:10:42 -0300

    inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio21/Exercicio21.java
@@ -0,0 +1,19 @@
+
+
+package com.mycompany.exercicio21;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rafae
+ */
+public class Exercicio21 {
+
+    public static void main(String[] args) {
+        for (int i = 34; i <= 57; i = i + 2){
+            JOptionPane.showMessageDialog(null, i);
+        }
+    }
+
+}
